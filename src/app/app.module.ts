4bfe5093ae8fceb21d30975525bf8b9dfd883e40
sagar.kedari <sagar.kedari@coditas.com>
2022-08-03 13:21:08 +0530

    UI and auto-carousel done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule} from '@angular/material/icon';
 import {MatTableModule} from '@angular/material/table';
+import {NgbCarouselModule} from '@ng-bootstrap/ng-bootstrap'
 
 @NgModule({
   declarations: [
@@ -34,7 +35,7 @@ import {MatTableModule} from '@angular/material/table';
     MatButtonModule,
     MatIconModule,
     MatTableModule,
-   
+    NgbCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/custom-components/home-news-feed/home-news-feed.component.ts
+++ src/app/custom-components/home-news-feed/home-news-feed.component.ts
@@ -4,47 +4,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-news-feed',
   templateUrl: './home-news-feed.component.html',
-  styleUrls: ['./home-news-feed.component.scss']
+  styleUrls: ['./home-news-feed.component.scss'],
 })
 export class HomeNewsFeedComponent implements OnInit {
+  constructor(private newsService: NewsService) {}
 
-  constructor(private newsService:NewsService) { }
-
-  category:string='top';
-  news:any;
+  category: string = 'top';
+  news: any;
   ngOnInit(): void {
     this.getNewsData();
   }
 
-  getNewsData(){
-    this.newsService.getData().subscribe((response:any)=>{
-      console.log('response from service',response);
-      console.log('results',response.results);
-      this.news=response['results'].filter((result:any)=> result.category[0]===this.category)
-      console.log('filtered news',this.news);
-        })
+  getNewsData() {
+    this.newsService.getData().subscribe((response: any) => {
+      console.log('response from service', response);
+      console.log('results', response.results);
+      this.news = response['results'].filter(
+        (result: any) => result.category[0] === this.category
+      );
+      console.log('filtered news', this.news);
+    });
   }
-  topClicked(){
-    this.category="top";
+  changeCategory(type: string) {
+    this.category = type;
     this.getNewsData();
   }
-  businessClicked(){
-    this.category="business";
-    this.getNewsData();
+  isActive(type:string){
+    return this.category===type ? 'active':'inactive';
   }
-  technologyClicked(){
-    this.category="technology";
-    this.getNewsData();
-  }
-  entertainmentClicked(){
-    this.category="entertainment";
-    this.getNewsData();
-  }
-  scienceClicked(){
-    this.category='science';
-    this.getNewsData();
-  }
-
-
-
 }
