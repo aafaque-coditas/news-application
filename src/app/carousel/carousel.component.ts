00886c4649ca380473ab661ec78932dd7c5ae06c
sagar.kedari <sagar.kedari@coditas.com>
2022-08-10 17:36:02 +0530

    commit

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,7 +1,6 @@
 import { trigger, transition, style, animate } from '@angular/animations';
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
-
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
@@ -20,18 +19,19 @@ export class CarouselComponent implements OnInit {
   @Input() slides!: any;
   @Input() carouselSlides!: any;
 
-  category: string = "top";
-  constructor() { }
+  category: string = 'top';
+  constructor() {}
 
   ngOnInit(): void {
     this.nextSlide();
   }
-  
+
   currentSlide = 0;
 
   onPreviousClick() {
     let previous = this.currentSlide - 1;
-    this.currentSlide = previous < 0 ? this.carouselSlides.length - 1 : previous;
+    this.currentSlide =
+      previous < 0 ? this.carouselSlides.length - 1 : previous;
   }
 
   onNextClick() {
@@ -44,15 +44,15 @@ export class CarouselComponent implements OnInit {
     setInterval(() => {
       next = this.currentSlide + 1;
       this.currentSlide = next === this.carouselSlides.length ? 0 : next;
-    }, 5000)
+    }, 5000);
   }
 
-  changeCurrentSlide(slideNum:number){
-    this.currentSlide=slideNum;
+  changeCurrentSlide(slideNum: number) {
+    this.currentSlide = slideNum;
   }
 
-  setActiveClass(slideNumber:number){
-    return this.currentSlide==slideNumber? 'active' : '';
+  setActiveClass(slideNumber: number) {
+    return this.currentSlide == slideNumber ? 'active' : '';
   }
 
   setMyStyle(slide: any) {
@@ -61,6 +61,4 @@ export class CarouselComponent implements OnInit {
     };
     return styles;
   }
-
-
 }
