dff267a5875f257f6174805bd918918e9d3ac10a
tejas-labde <tejas.labde@coditas.com>
2022-08-16 15:22:13 +0530

    onclicking news tile functionality added

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -8,31 +8,31 @@ import { Observable, Subject } from 'rxjs';
 })
 export class NewsService {
   searchBarValue: string = '';
-  keywordMatch:any=[];
-  newsCollection:any='';
-  adminEmail='admin@news.com';
-  adminPassword='12345678'
+  keywordMatch: any = [];
+  newsCollection: any = '';
+  adminEmail = 'admin@news.com';
+  adminPassword = '12345678'
 
   private filteredNews = new Subject<any>();
   filteredNews$ = this.filteredNews.asObservable();
 
-  constructor(private httpClient: HttpClient) { 
-    if(localStorage.getItem('localData')){
-      this.newsCollection=localStorage.getItem('localData');
+  constructor(private httpClient: HttpClient) {
+    if (localStorage.getItem('localData')) {
+      this.newsCollection = localStorage.getItem('localData');
     }
-    else{
-      this.httpClient.get('../../assets/dummy-news.json').subscribe((response:any)=>{
-        console.log('getting data from json',response);
-        this.newsCollection=response;
-        localStorage.setItem('localData',JSON.stringify(this.newsCollection));
+    else {
+      this.httpClient.get('../../assets/dummy-news.json').subscribe((response: any) => {
+
+        this.newsCollection = response;
+        localStorage.setItem('localData', JSON.stringify(this.newsCollection));
       })
     }
-    
-    
+
+
   }
 
   getData(): Observable<any> {
-    this.newsCollection=JSON.parse(localStorage.getItem('localData') || '');
+    this.newsCollection = JSON.parse(localStorage.getItem('localData') || '');
     return this.newsCollection;
   }
 
@@ -48,35 +48,33 @@ export class NewsService {
               news.keywords.map((keyword: string) => {
                 if (valueToBeSearched.toLowerCase() == keyword.toLowerCase()) {
                   this.keywordMatch.push(news);
-                  console.log('category matched');
+
                 }
               });
             }
             if (news.title.includes(valueToBeSearched)) {
               this.keywordMatch.push(news);
-              console.log('title matched');
+
             }
           }
         });
-        console.log('keyword matches', this.keywordMatch);
+
       });
     this.filteredNews.next(this.keywordMatch);
   }
 
   searchBarData() {
-    console.log('search bar data', this.searchBarValue);
     this.filteredNews.next(this.searchBarValue);
   }
 
 
-  addNews(newsValue:any):boolean{
-    console.log('service wali value',newsValue);
-    this.httpClient.get('../../assets/dummy-news.json').subscribe((response:any)=>{
-      console.log(response);
-      this.newsCollection=response;
+  addNews(newsValue: any): boolean {
+    console.log('service wali value', newsValue);
+    this.httpClient.get('../../assets/dummy-news.json').subscribe((response: any) => {
+      this.newsCollection = response;
       this.newsCollection.results.push(newsValue);
-      localStorage.setItem('localData',JSON.stringify(this.newsCollection));
-      console.log('updated array of news',this.newsCollection);
+      localStorage.setItem('localData', JSON.stringify(this.newsCollection));
+
       return true;
     })
     return false;
@@ -94,4 +92,5 @@ export class NewsService {
       return false;
     }
   }
+
 }
